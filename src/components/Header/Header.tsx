6e0fe2353ec3e11d7586325d951c3d14564db30d
Max Welin <max.welin@maiamarketing.se>
2026-01-08 09:49:25 +0100

    Header and footer added, styling done

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import styles from "./Header.module.css"
+import logo from "../../assets/adsmockup_logo.png"
+
+const Header = () => { 
+
+  return (
+    <header>
+        <img src={logo} alt={"Ads mockup logo"} />
+        <span><strong>ads</strong>mockup</span>
+    </header>
+  )
+}
+
+export default Header
