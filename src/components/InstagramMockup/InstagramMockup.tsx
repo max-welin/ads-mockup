6d0b82497f76a4e23a56ea778fe4c37d6253e726
Max Welin <max.welin@maiamarketing.se>
2026-02-04 08:53:51 +0100

    instagram mockup wip

--- src/components/InstagramMockup/InstagramMockup.tsx
+++ src/components/InstagramMockup/InstagramMockup.tsx
@@ -1,5 +1,28 @@
+import { useMockupContext } from "../../hooks/useMockupContext";
+import FacebookCtaSection from "../FacebookMockup/FacebookCtaSection/FacebookCtaSection";
+import FacebookHeaderSection from "../FacebookMockup/FacebookHeader/FacebookHeader";
+import FacebookTextSection from "../FacebookMockup/FacebookHeader/FacebookTextSection";
+import FacebookInteractionSection from "../FacebookMockup/FacebookInteractionSection/FacebookInteractionSection";
+import InstagramImgSection from "./InstagramImageSection/InstagramImageSection";
+import styles from "./InstagramMockup.module.css";
+
 const InstagramMockup = () => {
-  return <div>InstagramMockup</div>;
+  const { setShowImageButton } = useMockupContext();
+
+  return (
+    <div
+      className={styles.mockup}
+      onMouseEnter={() => setShowImageButton(true)}
+      onMouseLeave={() => setShowImageButton(false)}
+    >
+      <FacebookHeaderSection />
+      <FacebookTextSection />
+      <InstagramImgSection />
+      <FacebookCtaSection />
+
+      <FacebookInteractionSection />
+    </div>
+  );
 };
 
 export default InstagramMockup;
